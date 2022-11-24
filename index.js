1489f1088e9caf8cfbca02510ea6ba7bbb4c66cc
kchn9 <mat.kuchnia@gmail.com>
2022-11-24 18:38:53 +0100

    Pass missing app argument to httpServer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./app");
 const config = require("config");
 const logger = require("./utils/logger");
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(config.port, () => {
   logger.info(`Running server on port ${config.port}`);
