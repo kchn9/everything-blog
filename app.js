231c2cd9a14cf9c94f0167e800ce72bb1a772bfd
kchn9 <mat.kuchnia@gmail.com>
2022-11-24 18:38:14 +0100

    Add json parsing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { requestLogger } = require("./utils/middleware");
 const postsRouter = require("./controllers/posts");
 
 app.use(requestLogger);
+app.use(express.json());
 
 app.use("/api/v1/posts", postsRouter);
 
