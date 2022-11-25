829fc595a74d147d6ac95b9c6496821a06c46a3f
kchn9 <mat.kuchnia@gmail.com>
2022-11-25 11:30:07 +0100

    Move mongoose connection to app

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const {
+  db: { user, password, name },
+} = require("config");
+const dbUri = `mongodb+srv://${user}:${password}@cluster0.qlxz8um.mongodb.net/${name}?retryWrites=true&w=majority`;
+const postsRouter = require("./controllers/posts");
 const {
   requestLogger,
   errorHandler,
   unknownEndpointHandler,
 } = require("./utils/middleware");
-const postsRouter = require("./controllers/posts");
+
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    logger.info(`Succesfully connected to ${name} database`);
+  })
+  .catch((e) => {
+    logger.error("Error caught.");
+    logger.error(e);
+  });
 
 app.use(requestLogger);
 app.use(express.json());
--- models/post.js
+++ models/post.js
@@ -1,19 +1,4 @@
 const mongoose = require("mongoose");
-const logger = require("../utils/logger");
-const {
-  db: { user, password, name },
-} = require("config");
-const dbUri = `mongodb+srv://${user}:${password}@cluster0.qlxz8um.mongodb.net/${name}?retryWrites=true&w=majority`;
-
-mongoose
-  .connect(dbUri)
-  .then(() => {
-    logger.info(`Succesfully connected to ${name} database`);
-  })
-  .catch((e) => {
-    logger.error("Error caught.");
-    logger.error(e);
-  });
 
 const postSchema = new mongoose.Schema(
   {
